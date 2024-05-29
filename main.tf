35af1d4bb2517807ef6d3bb6fc90cadb578fe57b
Ammar <ammaralam37@gmail.com>
2024-05-29 11:19:57 -0400

    Add secret variables and configure provider

---
+++ main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+}
+
+provider "aws" {
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+resource "aws_s3_bucket" "www_site" {
+  bucket = "www.myskillfoliotest01"
+}
+
+resource "aws_s3_bucket" "redirect_site" {
+  bucket = "redirectmyskillfoliotest01"
+}
